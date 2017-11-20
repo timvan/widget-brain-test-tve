c516bbbf2279aa79c1021c7871007150695e5033
timvan <timvanellemeet@gmail.com>
2017-11-20 14:47:21 +0700

    added flush_jokes and fixed ICNDB non-calling error

--- flaskr.py
+++ flaskr.py
@@ -7,11 +7,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def display():
-	return "Hi, welcome to Chuck Norris joke machine"
+	init_db()
 	get_joke(10)
+	return "Hi, welcome to Chuck Norris joke machine"
 
 @app.route('/getJokes')
 def return_jokes():
+	"""read database for all jokes, return page with jokes seperated by html linespace
+	"""
 	page = ""
 	with open('./jokes_storage.csv', 'r') as f:
 		reader = csv.reader(f)
@@ -20,31 +23,49 @@ def return_jokes():
 			page += ((row)[1] + "<br />")
 	return (page)
 
+
+@app.route('/flushJokes')
+def flush_jokes():
+	"""Remove database entries by re-intialising the database
+	"""
+	init_db()
+	return "Flushed jokes: re-intiailised database"
+
+
 def chuck_call():
 	"""Call for Chuck Norris jokes
 	Return dictionary of Chuck Norris values
 	"""
+	print ("chuck_call")
 	url = 'http://api.icndb.com/jokes/random/'
 	response = requests.get(url)
 	data = response.json()
 	return data
 
+
 def get_joke(n):
 	"""Write to jokes_storage.csv
-	First erase and write headings to jokes_storage.csv
-	Second call for Chuck Norris jokes n times and append to ./jokes_storage.csv
+	Initialise the databse
+	Call for Chuck Norris jokes n times and append to ./jokes_storage.csv
 	"""
-	headings = ['id', 'joke', 'categories']
-	with open('./jokes_storage.csv', 'w') as f:
-		writer = csv.writer(f)
-		writer.writerow(headings)
-
+	print ("get_joke")
 	with open('./jokes_storage.csv', 'a') as f:
 		writer = csv.writer(f)
 		for i in range(n):
 			data = chuck_call()
 			writer.writerow([data['value']['id'], data['value']['joke'],
 			 	data['value']['categories']])
+
+
+def init_db():
+	"""Intialise the database with heading
+	"""
+	print ("init_db")
+	headings = ['id', 'joke', 'categories']
+	with open('./jokes_storage.csv', 'w') as f:
+		writer = csv.writer(f)
+		writer.writerow(headings)
+	return
 
 if __name__ == '__main__':
 	app.run(debug=True)
@@ -53,5 +74,8 @@ if __name__ == '__main__':
 TODO:
 
 - Check is call is succesfuly or not, only store if succes
+- for chuck_call - check / wait for a response
+- init_db should first check if exists, 
+- flushJokes - should delete database rows instead of re-initialising
 
 """
