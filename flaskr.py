006aafab990aa97cea8b4c81cd9d84e214a45445
timvan <timvanellemeet@gmail.com>
2017-11-20 13:52:57 +0700

    created endpoint './getJokes'  to return jokes and amended function ordering

--- flaskr.py
+++ flaskr.py
@@ -7,22 +7,51 @@ app = Flask(__name__)
 
 @app.route('/')
 def display():
-	chuck_call(10)
+	return "Hi, welcome to Chuck Norris joke machine"
+	get_joke(10)
 
+@app.route('/getJokes')
+def return_jokes():
+	page = ""
+	with open('./jokes_storage.csv', 'r') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			print (row[1])
+			page += ((row)[1] + "<br />")
+	return (page)
 
-def chuck_call(n):
+def chuck_call():
+	"""Call for Chuck Norris jokes
+	Return dictionary of Chuck Norris values
+	"""
 	url = 'http://api.icndb.com/jokes/random/'
-	for i in range(n):
-		response = requests.get(url)
-		html = response.content
-		store_joke_internally(html)
+	response = requests.get(url)
+	data = response.json()
+	return data
 
-def store_joke_internally(joke):
+def get_joke(n):
+	"""Write to jokes_storage.csv
+	First erase and write headings to jokes_storage.csv
+	Second call for Chuck Norris jokes n times and append to ./jokes_storage.csv
+	"""
+	headings = ['id', 'joke', 'categories']
+	with open('./jokes_storage.csv', 'w') as f:
+		writer = csv.writer(f)
+		writer.writerow(headings)
+
 	with open('./jokes_storage.csv', 'a') as f:
 		writer = csv.writer(f)
-		writer.writerow([joke])
-
+		for i in range(n):
+			data = chuck_call()
+			writer.writerow([data['value']['id'], data['value']['joke'],
+			 	data['value']['categories']])
 
 if __name__ == '__main__':
 	app.run(debug=True)
 
+"""
+TODO:
+
+- Check is call is succesfuly or not, only store if succes
+
+"""
