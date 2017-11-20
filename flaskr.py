383383c47d18aeda57ac98029367f3bd15ed209b
timvan <timvanellemeet@gmail.com>
2017-11-20 12:56:37 +0700

    web server calls chuck norris jokes and stores 10 copies in csv

--- flaskr.py
+++ flaskr.py
@@ -1,13 +1,27 @@
 import csv
-import urllib2
-
+import requests
 from flask import Flask
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def display():
-	return "it works!"
+	chuck_call(10)
+
+
+def chuck_call(n):
+	url = 'http://api.icndb.com/jokes/random/'
+	for i in range(n):
+		response = requests.get(url)
+		html = response.content
+		store_joke_internally(html)
+
+def store_joke_internally(joke):
+	with open('./jokes_storage.csv', 'a') as f:
+		writer = csv.writer(f)
+		writer.writerow([joke])
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
